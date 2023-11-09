347f35f066a036da847fcb92d33025a489e2e94c
Jake 'Sid' Smith <hello@jakesidsmith.com>
2023-11-09 14:58:04 +0000

    Rename nested layout

--- src/_not-included/index.layout.tsx
+++ src/_not-included/index.layout.tsx
@@ -6,6 +6,9 @@ const Layout = ({ children }: PropsWithChildren) => (
       This is part of a nested layout, which is applied in addition to the main
       layout.
     </p>
+    <p>
+      This layout will apply to any pages within the _not-included directory.
+    </p>
     {children}
   </>
 );
